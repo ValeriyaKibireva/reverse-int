49202b08ffbe8e6e33667c5b9acf96d455b7be9f
ValeriyaKibireva <kiborglera@gmail.com>
2023-02-15 15:21:25 +0100

    feat: complete task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
-module.exports = function reverse (n) {
-  
+module.exports = function reverse(n) {
+    const absoluteValue = Math.abs(n);
+    const getArr = absoluteValue.toString().split('');
+    let reverseArr = getArr.reverse();
+
+    if (reverseArr[0] === 0) {
+        reverseArr = reverseArr.shift();
+    }
+
+    return reverseArr.join('');
 }
+
